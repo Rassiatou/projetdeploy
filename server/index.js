9199670feff7ba5885275ef86dfd053956b8eb8d
Rassiatou Coulibaly <2437099@grasset.qc.ca>
2026-01-14 15:31:28 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import { db } from "./db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+// CORS DOIT ÊTRE ICI
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
+app.use(express.json());
+
+// Test API
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur l’API Express + MySQL !");
+});
+
+// USERS
+app.get("/users", (req, res) => {
+  const sql = "SELECT * FROM users";
+  db.query(sql, (err, data) => {
+    if (err) return res.status(500).json({ error: err.message });
+    return res.json(data);
+  });
+});
+
+app.post("/users", (req, res) => {
+  const { name, email } = req.body;
+  const sql = "INSERT INTO users (name, email) VALUES (?, ?)";
+  db.query(sql, [name, email], (err, result) => {
+    if (err) return res.status(500).json(err);
+    return res.json({ message: "Utilisateur ajouté avec succès !" });
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Serveur en ligne sur le port ${PORT}`));
